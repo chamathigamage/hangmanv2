b49307ea339b3a991dd3fba32c558e287939a6cf
ChamathiGamage <chamathi.gamage@stileeducation.com>
2022-06-01 14:37:00 +1000

    IT WORKS not check correct answer yet tho

--- src/index.js
+++ src/index.js
@@ -6,62 +6,68 @@ class WordBoxes extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            word: "hello",
             letterMatch: false
         }
     }
 
     render() {
         let letterArr = [];
-        for (let i = 0; i < this.state.word.length; i++)
-            letterArr.push(<Square value={this.state.word[i]} />);
+        for (let i = 0; i < this.props.word.length; i++)
+            letterArr.push(<Square userGuess={this.props.guess}correctWord={this.props.word} value={this.props.word[i]} correct={this.state.letterMatch} />);
         return letterArr
     }
 }
-// class Square extends React.Component{
-//     render() {
-//         return <div className="square">{this.props.value}</div>
-//     }
-// } 
 
-function Square(state) {
-    if (state.letterMatch) {
-        return <div className="square">{this.props.value}</div>;
+function Square(props) {
+    // if (props.values.includes(props.userGuess)) {
+    //     props.correct = true
+    // }
+    if (props.correct) {
+        return <div className="square">{props.value}</div>;
     } else {
         return <div className="square"></div>;
     }
 }
-class UserInput extends React.Component {
+class UserInput extends React.Component  {
     render() {
         return (
             <label>
             Input:
-            <input type="text" onKeyDown={this.props.f} />
+            <input type="text" onKeyDown={this.props.onclick} />
             </label>
         );
-        }}
- 
-  const GetInputValue = (event) => {
-    const input = [];
-    const value = event.target.value;
-    if (event.key === "Enter") {
-      input.push(value);
-      WordBox(input);
     }
-  };
-  const WordBox = (value) => {
-    const word = value;
-    console.log(word, word.length);
-  };
+}
+
+
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          userGuess: null,
+        };
+
+        this.word = "hello";
+    }
+    GetInputValue(event) {
+        let value = event.target.value;
+        var guess = [];
+        if (event.key === "Enter") {
+            event.target.value = ""
+            guess.push(value)
+        };
+        this.setState({
+            userGuess: guess
+        })
+    }
     render() {
       return (
         <div className="game">
           <div className="game-board">
-            <WordBoxes />
+                  <WordBoxes guess={this.state.userGuess} word={this.word}/>
           </div>
               <div className="game-info">
-                  <UserInput f={GetInputValue} />
+                  <UserInput onclick={(event) =>this.GetInputValue(event)}/>
           </div>
         </div>
       );
